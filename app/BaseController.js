001224454956af97b26c88ff662f5c013b064b36
bbilginn <bbilginn@gmail.com>
2014-02-01 19:36:26 +0200

    Hesaplama işlemleri.

--- app/BaseController.js
+++ app/BaseController.js
@@ -1,4 +1,5 @@
 ﻿function BaseController($scope, $http) {
+
     // Başlarken, Result değeri "0" olarak set edilir
     $scope.Result = 0;
 
@@ -12,16 +13,45 @@
 
     // Operatörler tıklandığında
     $scope.ClickOperator = function (Operator) {
-
+        $scope.First = $scope.Result
+        $scope.Operator = Operator;
+        $scope.Result = 0;
     }
 
     // Sonuç için "eşittir" işareti tıklandığında
     $scope.ClickResult = function () {
+        switch ($scope.Operator) {
+            case '*':
+                $scope.Result = $scope.First * $scope.Result;
+                break;
+            case '/':
+                if ($scope.Result != 0) $scope.Result = $scope.First / $scope.Result;
+                else alert("Sıfıra bölünemez!");
+                break;
+            case '+':
+                $scope.Result = $scope.First + $scope.Result;
+                break;
+            case '-':
+                $scope.Result = $scope.First - $scope.Result;
+                break;
+        }
+        FirstAndOperatorClear();
+    }
 
+    // Silme tuşu tıklandığında
+    $scope.ClickBackSpace = function () {
+        $scope.Result = $scope.Result.toString().substring(0, $scope.Result.toString().length - 1);
+        FirstAndOperatorClear();
     }
 
     // Temizleme tuşu tıklandığında
     $scope.ClickClear = function () {
+        $scope.Result = 0;
+        FirstAndOperatorClear();
+    }
 
+    function FirstAndOperatorClear() {
+        $scope.First = null;
+        $scope.Operator = null;
     }
 }
