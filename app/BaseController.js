d4ee8b895c254adc2e90568a5c1142da453b26cb
bbilginn <bbilginn@gmail.com>
2014-02-01 16:32:18 +0200

    Numeric tuşların basılabilir olması

---
+++ app/BaseController.js
@@ -0,0 +1,13 @@
+﻿function BaseController($scope, $http) {
+
+    $scope.Result = 0;
+
+
+    // Numaralar tıklandığında
+    $scope.Number = function (Number) {
+        if ($scope.Result == 0)
+            $scope.Result = $scope.Result + Number;
+        else
+            $scope.Result = $scope.Result + Number.toString();
+    }
+}
