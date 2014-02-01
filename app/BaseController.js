185183b4c0d11f44e8f16d6f5b08c1607a7782b3
bbilginn <bbilginn@gmail.com>
2014-02-01 17:52:56 +0200

    Tıklanma metodları tanımlandı.

--- app/BaseController.js
+++ app/BaseController.js
@@ -1,13 +1,27 @@
 ﻿function BaseController($scope, $http) {
-
+    // Başlarken, Result değeri "0" olarak set edilir
     $scope.Result = 0;
 
-
     // Numaralar tıklandığında
-    $scope.Number = function (Number) {
+    $scope.ClickNumber = function (Number) {
         if ($scope.Result == 0)
             $scope.Result = $scope.Result + Number;
         else
             $scope.Result = $scope.Result + Number.toString();
+    }
+
+    // Operatörler tıklandığında
+    $scope.ClickOperator = function (Operator) {
+
+    }
+
+    // Sonuç için "eşittir" işareti tıklandığında
+    $scope.ClickResult = function () {
+
+    }
+
+    // Temizleme tuşu tıklandığında
+    $scope.ClickClear = function () {
+
     }
 }
